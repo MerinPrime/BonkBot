36f7f0f5e4848d9fead867ecf5ad169126a12409
FotisDa <96889060+FotisDa@users.noreply.github.com>
2025-05-05 02:59:18 +0300

    Added example Rooms Watcher

---
+++ examples/rooms_watcher.py
@@ -0,0 +1,32 @@
+import asyncio
+
+from bonkbot.core.bonkbot import BonkBot
+from bonkbot.types import Mode
+
+"""
+This example implements a room watcher.
+The bot monitors all active rooms and logs only available VTOL rooms.
+"""
+
+bot = BonkBot()
+
+@bot.event
+async def on_ready():
+    print('Rooms watcher started')
+    
+    previous_rooms = set()
+    while True:
+        current_rooms = await bot.fetch_rooms()
+        current_rooms = set(
+            room for room in current_rooms
+            if room.mode == Mode.VTOL
+               and room.players < room.max_players
+               and not room.has_password
+        )
+        new_rooms = current_rooms - previous_rooms
+        for room in new_rooms:
+            print(f'New VTOL room: {room.name}')
+        previous_rooms = current_rooms
+        await asyncio.sleep(30)
+
+bot.login_as_guest('Kalalak')
