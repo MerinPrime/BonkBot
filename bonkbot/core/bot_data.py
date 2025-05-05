48c189e64f2dbc141aa3c43061064c3a0e247b6d
FotisDa <96889060+FotisDa@users.noreply.github.com>
2025-05-05 14:56:06 +0300

    Refactored

--- bonkbot/core/bot_data.py
+++ bonkbot/core/bot_data.py
@@ -22,25 +22,26 @@ class BotData:
 
     @staticmethod
     def from_login_response(json_data: dict) -> 'BotData':
-        data = BotData()
-        data.name = json_data['username']
-        data.token = json_data['token']
-        data.dbid = json_data['id']
-        data.xp = json_data['xp']
-        data.avatar = Avatar.from_buffer(ByteBuffer().from_base64(json_data['avatar'], uri_encoded=True))
-        data.active_avatar = json_data['activeAvatarNumber']
-        data.avatars = [
-            Avatar.from_buffer(ByteBuffer().from_base64(json_data['avatar1'], uri_encoded=True)),
-            Avatar.from_buffer(ByteBuffer().from_base64(json_data['avatar2'], uri_encoded=True)),
-            Avatar.from_buffer(ByteBuffer().from_base64(json_data['avatar3'], uri_encoded=True)),
-            Avatar.from_buffer(ByteBuffer().from_base64(json_data['avatar4'], uri_encoded=True)),
-            Avatar.from_buffer(ByteBuffer().from_base64(json_data['avatar5'], uri_encoded=True))
-        ]
-        data.friends = [Friend(
-            name=friend['name'],
-            dbid=friend['id'],
-            room_id=friend['roomid']
-        ) for friend in json_data['friends']]
-        data.legacy_friends = [Friend(name=friend, dbid=None, room_id=None) for friend in json_data['legacyFriends'].split('#')]
-        data.settings = Settings.from_buffer(ByteBuffer().from_base64(json_data['controls'], uri_encoded=False))
+        data = BotData(
+            name = json_data['username'],
+            token = json_data['token'],
+            dbid = json_data['id'],
+            xp = json_data['xp'],
+            avatar = Avatar.from_buffer(ByteBuffer().from_base64(json_data['avatar'], uri_encoded=True)),
+            active_avatar = json_data['activeAvatarNumber'],
+            avatars = [
+                Avatar.from_buffer(ByteBuffer().from_base64(json_data['avatar1'], uri_encoded=True)),
+                Avatar.from_buffer(ByteBuffer().from_base64(json_data['avatar2'], uri_encoded=True)),
+                Avatar.from_buffer(ByteBuffer().from_base64(json_data['avatar3'], uri_encoded=True)),
+                Avatar.from_buffer(ByteBuffer().from_base64(json_data['avatar4'], uri_encoded=True)),
+                Avatar.from_buffer(ByteBuffer().from_base64(json_data['avatar5'], uri_encoded=True))
+            ],
+            friends = [Friend(
+                name=friend['name'],
+                dbid=friend['id'],
+                room_id=friend['roomid']
+            ) for friend in json_data['friends']],
+            legacy_friends = [Friend(name=friend, dbid=None, room_id=None) for friend in json_data['legacyFriends'].split('#')],
+            settings = Settings.from_buffer(ByteBuffer().from_base64(json_data['controls'], uri_encoded=False))
+        )
         return data
