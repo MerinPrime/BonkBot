d0c78838601363456bffb6adeaadb08d3a22d9a8
FotisDa <96889060+MerinPrime@users.noreply.github.com>
2025-05-18 14:22:13 +0300

    Update mirror_bot.py

--- examples/mirror_bot.py
+++ examples/mirror_bot.py
@@ -3,11 +3,11 @@ from typing import TYPE_CHECKING
 
 from bonkbot.core.bot.bot import BonkBot
 from bonkbot.types import Inputs
+from bonkbot.types.player_move import PlayerMove
 
 if TYPE_CHECKING:
     from bonkbot.core import Room
     from bonkbot.core.player import Player
-    from bonkbot.types.player_move import PlayerMove
 
 '''
 This example implements a MirrorBot.
@@ -16,34 +16,30 @@ Modes like VTOL and Arrows can cause an "unmirror".
 '''
 
 class MirrorBot(BonkBot):
-    def __init__(self) -> None:
-        event_loop = asyncio.get_event_loop()
-        super().__init__(event_loop)
-        self.room = None
-
     async def on_ready(self, bot: 'BonkBot') -> None:
         print('MirrorBot started')
         await self.update_server()
-        self.room = bot.create_room(name='MirrorBot', unlisted=True)
-        await self.room.connect()
-        await self.room.wait_for_connection()
-        print(f'Link: {self.room.join_link}')
+        room = self.create_room(name='MirrorBot', unlisted=True)
+        await room.connect()
+        await room.wait_for_connection()
+        print(f'Link: {room.join_link}')
 
     async def on_player_join(self, room: 'Room', player: 'Player') -> None:
-        if room.bot_player.is_host:
-            await asyncio.sleep(2)
+        if room.bot_player.is_host or player.is_left:
+            await asyncio.sleep(5)
             await player.give_host()
 
     async def on_host_left(self, room: 'Room', player: 'Player') -> None:
-        if room.bot_player.is_host:
-            await asyncio.sleep(2)
-            if room.player_count == 1:
-                return
-            for player in room.players:
-                if player.is_bot:
-                    continue
-                await player.give_host()
-                break
+        if not room.bot_player.is_host:
+            return
+        if room.player_count == 1:
+            return
+        await asyncio.sleep(5)
+        for player in room.players:
+            if player.is_bot or player.is_left:
+                continue
+            await player.give_host()
+            break
 
     async def on_player_move(self, room: 'Room', player: 'Player', move: 'PlayerMove') -> None:
         frame = move.frame
@@ -62,13 +58,12 @@ class MirrorBot(BonkBot):
         await room.move(frame, inputs)
 
     async def on_move_revert(self, room: 'Room', player: 'Player', move: 'PlayerMove') -> None:
-        candidates = [s for s in room.bot_player.moves if s < move.sequence]
+        candidates = [bot_move for bot_move in room.bot_player.moves.values() if bot_move.frame < move.frame]
         if not candidates:
             last_inputs = Inputs()
             sequence = None
         else:
-            closest_key = max(candidates)
-            last_move = room.bot_player.moves[closest_key]
+            last_move = max(candidates, key=lambda move: move.frame)
             last_inputs = last_move.inputs
             sequence = last_move.sequence
         await room.move(move.frame, last_inputs, sequence)
