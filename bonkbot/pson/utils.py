9d73f875996e44b9602a7dc3f5be65496a313249
FotisDa <96889060+MerinPrime@users.noreply.github.com>
2025-06-04 00:24:41 +0300

    Fixed zigzag encoding/decoding

--- bonkbot/pson/utils.py
+++ bonkbot/pson/utils.py
@@ -1,14 +1,11 @@
 def zigzag_encode32(value: int) -> int:
-    return (((value | 1) << 1) ^ (value >> 31)) & 0xFFFFFFFF
-
+    return (value << 1) ^ (value >> 31) & 0xFFFFFFFF
 
 def zigzag_decode32(value: int) -> int:
-    return ((value >> 1) ^ -(value & 1)) & 0xFFFFFFFF
-
+    return (value >> 1) ^ -(value & 1) & 0xFFFFFFFF
 
 def zigzag_encode64(value: int) -> int:
-    return (((value | 1) << 1) ^ (value >> 63)) & 0xFFFFFFFFFFFFFFFF
-
+    return (value << 1) ^ (value >> 63) & 0xFFFFFFFFFFFFFFFF
 
 def zigzag_decode64(value: int) -> int:
-    return ((value >> 1) ^ -(value & 1)) & 0xFFFFFFFFFFFFFFFF
+    return (value >> 1) ^ -(value & 1) & 0xFFFFFFFFFFFFFFFF
