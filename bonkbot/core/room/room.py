4bdb497a6c5042682a4ac46a49def0eddbc858a4
FotisDa <96889060+MerinPrime@users.noreply.github.com>
2025-06-14 05:09:31 +0300

    Update room.py

--- bonkbot/core/room/room.py
+++ bonkbot/core/room/room.py
@@ -323,8 +323,8 @@ class Room:
             data['token'] = self.bot.token
         self._room_data = RoomData(
             name=self._room_params.name,
-            host=self._bot_player,
-            players=[self._bot_player],
+            host=None,
+            players=[],
             map=BonkMap.decode_from_database('ILAcJAhBFBjBzCIDCAbAcgBwEYA1IDOAWgMrAAeAJgFYCiwytlAjEQGLoAMsAtm50gCmAdwbBIbACoBDAOrNh2AOIBVeAFlcATXIBJZAAtURJak4BpaMAASJAExsCW2eQPTRkACJFdITwDMANRB6RhZ2Ll5+JCgAdhjgX08PGKsYa0gE8WB0LLz8goKrCGZA7B4AVgNsWUCAa10OAHstfFR-AGoAeh7envAbLoA3Pr7O0d7waWxMOyzM4DYALxBhKjp4FSVXSiUiId4BQuO8roAWfOQugYTPLsl1JcfnlZO394-Pk7TgaFpMv4QegQZDCNh1LKeYAAeWKXwKMH+vyQgUksCUbAAzNg6pAiHlhJ4IfDCioAcCQGwVJjIAZKHYLkggA'),
         )
         await self._socket.emit(SocketEvents.Outgoing.JOIN_ROOM, data)
