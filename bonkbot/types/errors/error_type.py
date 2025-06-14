96888da8a9e2a90bb43e30bbb66a2d1c172dedf9
FotisDa <96889060+MerinPrime@users.noreply.github.com>
2025-06-14 05:21:11 +0300

    Update error_type.py

--- bonkbot/types/errors/error_type.py
+++ bonkbot/types/errors/error_type.py
@@ -2,19 +2,19 @@ import enum
 
 
 class ErrorType(enum.Enum):
-    RATE_LIMITED = 'ratelimited'
-    PASSWORD = 'password'
-    USERNAME_FAIL = 'username_fail'
-    NOT_HOST = 'not_host'
-    INVALID_BALANCE = 'invalid_balance'
-    ROOM_NOT_FOUND = 'roomnotfound'
-    HOST_CHANGE_RATE_LIMITED = 'host change rate limited'
-    NEW_HOST_NOT_PRESENT = 'new_host_not_present'
-    AVATAR_DATA_INVALID = 'avatar_data_invalid'
-
+    RATE_LIMITED = ('ratelimited',)
+    PASSWORD = ('password',)
+    USERNAME_FAIL = ('username_fail',)
+    NOT_HOST = ('not_host',)
+    INVALID_BALANCE = ('invalid_balance',)
+    ROOM_NOT_FOUND = ('roomnotfound','room_not_found')
+    HOST_CHANGE_RATE_LIMITED = ('host change rate limited',)
+    NEW_HOST_NOT_PRESENT = ('new_host_not_present',)
+    AVATAR_DATA_INVALID = ('avatar_data_invalid',)
+    
     @staticmethod
     def from_string(code: str) -> 'ErrorType':
         for error_type in ErrorType:
-            if error_type.value == code:
+            if code in error_type.value:
                 return error_type
         raise ValueError(f'{code} is not a valid error code')
