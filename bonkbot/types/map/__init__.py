b4b0bf9814196b4a256550748217ace996d885db
FotisDa <96889060+MerinPrime@users.noreply.github.com>
2025-07-02 00:42:07 +0300

    Update __init__.py

--- bonkbot/types/map/__init__.py
+++ bonkbot/types/map/__init__.py
@@ -1,6 +1,7 @@
 from . import physics
 from .bonkmap import BonkMap
 from .capture_zone import CaptureZone
+from .capture_type import CaptureType
 from .map_metadata import MapMetadata
 from .map_properties import MapProperties
 from .spawn import Spawn
