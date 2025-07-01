8c41ebb8b54bcb7a6504c9c1be7b73c40bf3d08e
FotisDa <96889060+MerinPrime@users.noreply.github.com>
2025-07-02 00:54:47 +0300

    Refactor Friend
    Change dataclass to attrs

--- bonkbot/types/friend.py
+++ bonkbot/types/friend.py
@@ -1,9 +1,14 @@
 from dataclasses import dataclass
 from typing import Optional
 
+from attrs.setters import frozen
+from attrs import define, field
 
-@dataclass(frozen=True)
+from ..utils.validation import validate_opt_int, validate_str
+
+
+@define(slots=True, auto_attribs=True)
 class Friend:
-    name: str = ''
-    dbid: Optional[int] = None
-    room_id: Optional[int] = None
+    name: str = field(validator=validate_str(15))
+    dbid: Optional[int] = field(default=None, validator=validate_opt_int(0), on_setattr=frozen)
+    room_id: Optional[int] = field(default=None, validator=validate_opt_int(0, 999999), on_setattr=frozen)
