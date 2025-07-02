74c0b6c05aae7cae5e844b0df411db4442f5de7a
FotisDa <96889060+MerinPrime@users.noreply.github.com>
2025-07-03 02:09:14 +0300

    Add validate and convert vector list

--- bonkbot/utils/validation.py
+++ bonkbot/utils/validation.py
@@ -1,4 +1,4 @@
-from typing import Any, Callable, Iterable, Optional, Tuple, Union
+from typing import Any, Callable, Iterable, List, Optional, Tuple, Union
 
 import attrs
 
@@ -6,11 +6,14 @@ import attrs
 def convert_to_float_vector(value: Tuple[Any, Any]) -> Tuple[float, float]:
     if not isinstance(value, (tuple, list)) or len(value) != 2:
         raise TypeError(f'Position must be a tuple or list of two elements, but got {type(value)}')
-    
+
     try:
         return float(value[0]), float(value[1])
     except (ValueError, TypeError) as e:
         raise TypeError(f'Could not convert position elements to float: {value}.\nErr: {e}') from e
+
+def convert_to_float_vector_list(value: List[Tuple[Any, Any]]) -> List[Tuple[float, float]]:
+    return list(map(convert_to_float_vector, value))
 
 def convert_to_float_or_none(value: Optional[Union[int, float, str]]) -> Optional[float]:
     if value is None:
@@ -22,19 +25,23 @@ def convert_to_float_or_none(value: Optional[Union[int, float, str]]) -> Optiona
         raise TypeError(f'Attribute must be type of float or None, given: {type(value).__name__}') from e
 
 
-def validate_vector_range(min_value: float, max_value: float) -> None:
+def validate_vector_range(min_value: float, max_value: float) -> Callable[[Any, attrs.Attribute, Any], None]:
     def validate_vector_range(instance: Any, attribute: attrs.Attribute, value: Tuple[float, float]) -> None:
         if len(value) != 2:
             raise ValueError(f"'{attribute.name}' must be a tuple of length 2, but got {value}")
-        
+
         x, y = value
         if not min_value <= x <= max_value:
             raise ValueError(f"'{attribute.name}' x-coordinate ({x}) is out of the valid range [{min_value}, {max_value}]")
-    
+
         if not min_value <= y <= max_value:
             raise ValueError(f"'{attribute.name}' y-coordinate ({y}) is out of the valid range [{min_value}, {max_value}]")
-    
+
     return validate_vector_range
+
+
+def validate_vector_list_range(min_value: float, max_value: float) -> Callable[[Any, attrs.Attribute, Any], None]:
+    return attrs.validators.deep_iterable(validate_vector_range(min_value, max_value))
 
 def validate_int(min_value: Optional[int] = None,
                  max_value: Optional[int] = None) -> Callable[[Any, attrs.Attribute, Any], None]:
