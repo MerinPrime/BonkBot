ab65fae36368747214ea69c4437731b95ff4b137
FotisDa <96889060+MerinPrime@users.noreply.github.com>
2025-07-04 04:36:40 +0300

    Add mode id

--- bonkbot/types/mode.py
+++ bonkbot/types/mode.py
@@ -6,17 +6,18 @@ from dataclasses import dataclass
 class GaMo:
     engine: str
     mode: str
+    id: int
 
 # Source: https://github.com/MerinPrime/ReBonk/blob/master/src/core/GameSettings.ts
 class Mode(enum.Enum):
-    NONE = GaMo('', '')
-    CLASSIC = GaMo(engine='b', mode='b')
-    SIMPLE = GaMo(engine='b', mode='bs')
-    ARROWS = GaMo(engine='b', mode='ar')
-    DEATH_ARROWS = GaMo(engine='b', mode='ard')
-    GRAPPLE = GaMo(engine='b', mode='sp')
-    VTOL = GaMo(engine='b', mode='v')
-    FOOTBALL = GaMo(engine='f', mode='f')
+    NONE = GaMo('', '', 0)
+    CLASSIC = GaMo(engine='b', mode='b', id=1)
+    SIMPLE = GaMo(engine='b', mode='bs', id=2)
+    ARROWS = GaMo(engine='b', mode='ar', id=3)
+    DEATH_ARROWS = GaMo(engine='b', mode='ard', id=4)
+    GRAPPLE = GaMo(engine='b', mode='sp', id=5)
+    VTOL = GaMo(engine='b', mode='v', id=6)
+    FOOTBALL = GaMo(engine='f', mode='f', id=7)
 
     @property
     def engine(self) -> str:
@@ -26,9 +27,20 @@ class Mode(enum.Enum):
     def mode(self) -> str:
         return self.value.mode
 
+    @property
+    def id(self) -> str:
+        return self.value.id
+
     @staticmethod
     def from_mode_code(string: str) -> 'Mode':
         for mode in Mode:
             if mode.mode == string:
                 return mode
         raise ValueError(f'Invalid mode code: {string}')
+
+    @staticmethod
+    def from_mode_id(mode_id: int) -> 'Mode':
+        for mode in Mode:
+            if mode.id == mode_id:
+                return mode
+        raise ValueError(f'Invalid mode id: {mode_id}')
