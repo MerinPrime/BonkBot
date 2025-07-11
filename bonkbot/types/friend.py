3e42f2b59b5a561434d330f0ea9c140603bda4f4
Merin <96889060+MerinPrime@users.noreply.github.com>
2025-07-11 12:43:05 +0300

    Change frozen fields to class

--- bonkbot/types/friend.py
+++ bonkbot/types/friend.py
@@ -1,13 +1,12 @@
 from typing import Optional
 
 from attrs import define, field
-from attrs.setters import frozen
 
 from ..utils.validation import validate_opt_int, validate_str
 
 
-@define(slots=True, auto_attribs=True)
+@define(slots=True, auto_attribs=True, frozen=True)
 class Friend:
     name: str = field(validator=validate_str(15))
-    dbid: Optional[int] = field(default=None, validator=validate_opt_int(0), on_setattr=frozen)
-    room_id: Optional[int] = field(default=None, validator=validate_opt_int(0), on_setattr=frozen)
+    dbid: Optional[int] = field(default=None, validator=validate_opt_int(0))
+    room_id: Optional[int] = field(default=None, validator=validate_opt_int(0))
